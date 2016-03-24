e5597d06236b90e53600729d970d0ed47823c992
Kanit Srisuthep <kanit9618@gmail.com>
2016-03-24 07:42:02 -0700

    nlp

---
+++ Html.py
@@ -0,0 +1,120 @@
+import urllib2
+import re
+from bs4 import BeautifulSoup
+from Nlp import *
+import bs4
+import sys
+sys.setrecursionlimit(50000)
+
+#----- configurations
+_ratio = 0.75
+
+class Html:
+	def __init__(self,url):
+		try:
+			opener = urllib2.build_opener()
+			opener.addheaders = [('Accept-Charset', 'utf-8')]
+			content = opener.open(url, timeout=5).read()
+		except:
+			raise ValueError('Unable to get content from '+url)
+		
+		try:
+			soup = BeautifulSoup(content)
+			soup.encode('utf-8')
+			[s.replaceWith(" ") for s in soup('script')]
+		except:
+			raise ValueError('Unable to parse the content')
+		[s.replaceWith(" ") for s in soup('link')]
+		[s.replaceWith(" ") for s in soup('style')]
+
+		invalid_tags = ['b', 'i', 'u', 'strong', 'a']
+		for tag in invalid_tags: 
+			for match in soup.findAll(tag):
+				match.replaceWithChildren()
+
+		self.soup = soup;
+		pass;
+
+	def parseNode(self,node):
+		# for r in node:
+		# 	print r.encode('utf-8','ignore')
+		# 	if (r.string is None):
+		# 		r.string = ' '
+		return parseText(node.get_text())
+
+	def getTitle(self):
+		if(self.soup.title == None):
+			return "";
+		else:
+			txt = self.parseNode(self.soup.find('title'))
+			return re.sub(r'\W+', ' ', txt)
+
+	def getKeyword(self):
+		tag = self.soup.find("meta", {"name":"keywords"})
+		if(tag== None):
+			return "";
+		else:
+			return parseText(tag['content']);
+
+	def getHx(self):
+		ans = []
+
+		tag = self.soup.find("h1")
+		if(tag != None):
+			ans.append(self.parseNode(tag))
+
+		tag = self.soup.find("h2")
+		if(tag != None):
+			ans.append(self.parseNode(tag))
+
+		tag = self.soup.find("h3")
+		if(tag != None):
+			ans.append(self.parseNode(tag))
+
+		tag = self.soup.find("h4")
+		if(tag != None):
+			ans.append(self.parseNode(tag))
+
+
+		return ans;
+
+	def getContent(self):
+		ans=[]
+		str = ""
+		self.getContentRes(None,ans)
+		for txt in ans: 
+			str+= txt+" "
+		return parseText(str)
+
+	def getContentRes(self,node,ans):
+		if(node == None):
+			node = self.soup
+		
+		if(len(str(node)) == 0):
+			return
+		ratio = len(node.get_text())/float(len(str(node)))
+		if(ratio > _ratio):
+			# print "\n\n\n\n\n\n\n--"
+			# print self.parseNode(node)+" "
+			# print ratio
+			# print "\n\n\n\n\n\n\n--"
+			ans.append(self.parseNode(node)+" ")
+		else:
+			for i in range(len(node.findChildren(recursive = False))):
+				self.getContentRes(node.findChildren(recursive = False)[i],ans)
+	
+
+# for testing purpose
+if __name__ == "__main__":
+	# h = Html('http://www.amazon.com/Cuisinart-CPT-122-Compact-2-Slice-Toaster/dp/B009GQ034C/ref=sr_1_1?s=kitchen&ie=UTF8&qid=1431620315&sr=1-1&keywords=toaster')
+	# h = Html('http://blog.rei.com/camp/how-to-introduce-your-indoorsy-friend-to-the-outdoors/')
+	# h = Html('http://www.cnn.com/2013/06/10/politics/edward-snowden-profile/')
+	
+	h = Html('https://www.airbnb.com/')
+	print "title:\t",h.getTitle()
+	print "keyword:\t",h.getKeyword()
+	print "hx:\t",h.getHx()
+	print "content:\t"
+	print h.getContent()
+
+
--- main.py
+++ main.py
@@ -1,17 +1,64 @@
-# coding=utf-8
-from Html import Html 
+from Html import *
+from Nlp import *
 import sys
 
 # --- configurations 
-url = 'http://www.hello.com'
+# url = 'http://www.hello.com'
+# url = 'http://www.amazon.com/Cuisinart-CPT-122-Compact-2-Slice-Toaster/dp/B009GQ034C/ref=sr_1_1?s=kitchen&ie=UTF8&qid=1431620315&sr=1-1&keywords=toaster'
+# url = 'http://blog.rei.com/camp/how-to-introduce-your-indoorsy-friend-to-the-outdoors/'
+# url = 'http://www.cnn.com/2013/06/10/politics/edward-snowden-profile/'
+# url = 'http://www.theverge.com/2016/3/24/11291380/roku-tcl-tvs-availability-price'
+# url = 'http://fathomaway.com/postcards/quirk/best-travel-blogs-and-websites/'
+# url = 'http://www.geeksforgeeks.org/top-10-algorithms-in-interview-questions/'
+# url = 'http://www-bcf.usc.edu/~feisha/'
+# url = 'http://usc-actlab.github.io/'
+# url = 'https://www.youtube.com/watch?v=px9iHkA0nOI&feature=youtu.be'
+# url = 'http://abcnews.go.com/Politics/donald-trump-targets-ted-cruzs-wife-twitter/story?id=37889421'
+# url = 'http://abcnews.go.com/Technology/wireStory/man-predicted-space-shuttle-challenger-disaster-dies-37881707'
+# url = 'http://www.dailynews.com/government-and-politics/20160323/bernie-sanders-draws-big-crowd-at-las-wiltern-theater'
+# url = 'http://www.amazon.com/gp/product/B00XGUXPVW/ref=s9_simh_gw_g79_i2_r?ie=UTF8&fpl=fresh&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-3&pf_rd_r=153M9PX3TW6EFWYM3XVM&pf_rd_t=36701&pf_rd_p=2437869462&pf_rd_i=desktop'
+url = 'https://vimeo.com/56438232'
+# -- todo 
+# stemming
+# remove stop words
+# consecutive words i.e. this is a cat<strong>!</strong> shall be translated to this is a ***cat !***
+# we may translate you'll to you will
+# ycombinator ? 
 
+
+# --- inputs 
+sys.argv
 
 # --- coding 
 try:
-	html = Html(url)	
+	h = Html(url)	
 except ValueError as e :
 	print e
 	sys.exit()
 
-print html.getTitle()
-# print soup.find("meta", {"name":"keywords"})
+title = h.getTitle()
+keyword = h.getKeyword()
+hx = h.getHx()
+
+content = h.getContent()
+content = oneGram(content)
+content = removeStopword(content)
+content = sortBag(content)
+content = content[-5:]
+
+totalword = title + " " + keyword
+for h in hx: totalword += " "+h
+totalword = oneGram(totalword)
+totalword = removeStopword(totalword)
+totalword = sortBag(totalword)
+totalword = totalword[-5:]
+
+finalize = mergeBag(totalword,content,3)
+finalize = sortBag(finalize)
+finalize = list(finalize[-5:])
+
+# print finalize
+print 'keywords : ',
+for w in finalize:
+	print w[0],
+print ''
